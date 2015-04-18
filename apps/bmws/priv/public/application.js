6de0d47dc67100253d93eac5838ccfc2542d413a
Alexey Nikitin <nikitin@dreamindustries.co>
2015-04-19 01:20:53 +0300

    Cleanup frontend a little

--- apps/bmws/priv/public/application.js
+++ apps/bmws/priv/public/application.js
@@ -3,33 +3,32 @@ $(document).ready(init);
 
 function init() {
   $('#server').val("ws://" + window.location.host + "/websocket");
-  if(!("WebSocket" in window)){
-    $('#status').append('<p><span style="color: red;">websockets are not supported </span></p>');
-    $("#navigation").hide();
-  } else {
+  if ("WebSocket" in window) {
     $('#status').append('<p><span style="color: green;">websockets are supported </span></p>');
     connect();
+  } else {
+    $('#status').append('<p><span style="color: red;">websockets are not supported </span></p>');
+    $("#navigation").hide();
   };
   $("#connected").hide();
   $("#content").hide();
 };
 
-function connect()
-{
+function connect() {
   wsHost = $("#server").val()
   websocket = new WebSocket(wsHost);
   showScreen('<b>Connecting to: ' +  wsHost + '</b>');
-  websocket.onopen = function(evt) { onOpen(evt) };
-  websocket.onclose = function(evt) { onClose(evt) };
-  websocket.onmessage = function(evt) { onMessage(evt) };
-  websocket.onerror = function(evt) { onError(evt) };
+  websocket.onopen    = function(event) { onOpen(event)    };
+  websocket.onclose   = function(event) { onClose(event)   };
+  websocket.onmessage = function(event) { onMessage(event) };
+  websocket.onerror   = function(event) { onError(event)   };
 };
 
 function disconnect() {
   websocket.close();
 };
 
-function toggle_connection(){
+function toggle_connection() {
   if(websocket.readyState == websocket.OPEN){
     disconnect();
   } else {
@@ -49,30 +48,29 @@ function sendTxt() {
   };
 };
 
-function onOpen(evt) {
+function onOpen(event) {
   showScreen('<span style="color: green;">CONNECTED </span>');
   $("#connected").fadeIn('slow');
   $("#content").fadeIn('slow');
 };
 
-function onClose(evt) {
+function onClose(event) {
   showScreen('<span style="color: red;">DISCONNECTED </span>');
 };
 
-function onMessage(evt) {
-  var resp = JSON.parse(evt.data);
+function onMessage(event) {
+  var resp = JSON.parse(event.data);
   showScreen('<span style="color: blue;">' + resp.message + ' from ' + resp.from + '</span>');
 };
 
-function onError(evt) {
-  showScreen('<span style="color: red;">ERROR: ' + evt.data+ '</span>');
+function onError(event) {
+  showScreen('<span style="color: red;">ERROR: ' + event.data+ '</span>');
 };
 
 function showScreen(txt) {
   $('#output').prepend('<p>' + txt + '</p>');
 };
 
-function clearScreen()
-{
+function clearScreen() {
   $('#output').html("");
 };
