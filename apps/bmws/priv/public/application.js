b2fdc7f6364e91462c72d6cca7dfd1acb3e70635
Vlad Syabruk <sjabrik@gmail.com>
2015-04-19 15:29:48 +0300

    Improve js

--- apps/bmws/priv/public/application.js
+++ apps/bmws/priv/public/application.js
@@ -1,76 +1,98 @@
+function Bmws() {
+  this.init = function() {
+    $('#server').val(this.userHost());
+    if ("WebSocket" in window) {
+      $('#status').append('<p><span style="color: green;">websockets are supported </span></p>');
+      this.connect();
+    } else {
+      $('#status').append('<p><span style="color: red;">websockets are not supported </span></p>');
+      $("#navigation").hide();
+    };
+    $("#connected").hide();
+    $("#content").hide();
+  };
+
+  this.login = function() {
+    return $("#login").val();
+  };
+
+  this.serverHost = function() {
+    return "ws://" + window.location.host + "/websocket";
+  };
+
+  this.userHost = function() {
+    if (this.login()) {
+      return [this.serverHost(), "users", this.login()].join("/");
+    } else {
+      return this.serverHost();
+    };
+  };
+
+  this.connect = function() {
+    this.showScreen('<b>Connecting to: ' + this.userHost() + '</b>');
+    websocket = new WebSocket(this.userHost());
+    websocket.onopen    = function(event) { bmws.onOpen(event)    };
+    websocket.onclose   = function(event) { bmws.onClose(event)   };
+    websocket.onmessage = function(event) { bmws.onMessage(event) };
+    websocket.onerror   = function(event) { bmws.onError(event)   };
+  };
+
+  this.disconnect = function() {
+    websocket.close();
+  };
+
+  this.toggle_connection = function() {
+    if(websocket.readyState == websocket.OPEN){
+      this.disconnect();
+    } else {
+      this.connect();
+    };
+  };
+
+  this.sendTxt = function() {
+    if(websocket.readyState == websocket.OPEN) {
+      var recepient = $("#recepient").val();
+      var message = $("#message").val();
+      var resp = {"message": message, "recepient": recepient};
+      websocket.send(JSON.stringify(resp));
+      this.showScreen('<span style="color: red;">' + this.login() + '</span>: ' + resp.message + '<br>');
+      $("#message").val("");
+    } else {
+      this.showScreen('websocket is not connected');
+    };
+  };
+
+  this.onOpen = function(event) {
+    this.showScreen('<span style="color: green;">CONNECTED </span>');
+    $("#connected").fadeIn('slow');
+    $("#content").fadeIn('slow');
+  };
+
+  this.onClose = function(event) {
+    this.showScreen('<span style="color: red;">DISCONNECTED </span>');
+  };
+
+  this.onMessage = function(event) {
+    var resp = JSON.parse(event.data);
+    this.showScreen('<span style="color: blue;">' + resp.from + '</span>: ' + resp.message);
+  };
+
+  this.onError = function(event) {
+    this.showScreen('<span style="color: red;">ERROR: ' + event.data+ '</span>');
+  };
+
+  this.showScreen = function(txt) {
+    $('#output').prepend('<p>' + txt + '</p>');
+  };
+
+  this.clearScreen = function() {
+    $('#output').html("");
+  };
+};
+
 var websocket;
-$(document).ready(init);
+var bmws = new Bmws();
 
-function init() {
-  $('#server').val("ws://" + window.location.host + "/websocket");
-  if ("WebSocket" in window) {
-    $('#status').append('<p><span style="color: green;">websockets are supported </span></p>');
-    connect();
-  } else {
-    $('#status').append('<p><span style="color: red;">websockets are not supported </span></p>');
-    $("#navigation").hide();
-  };
-  $("#connected").hide();
-  $("#content").hide();
-};
-
-function connect() {
-  wsHost = $("#server").val()
-  websocket = new WebSocket(wsHost);
-  showScreen('<b>Connecting to: ' +  wsHost + '</b>');
-  websocket.onopen    = function(event) { onOpen(event)    };
-  websocket.onclose   = function(event) { onClose(event)   };
-  websocket.onmessage = function(event) { onMessage(event) };
-  websocket.onerror   = function(event) { onError(event)   };
-};
-
-function disconnect() {
-  websocket.close();
-};
-
-function toggle_connection() {
-  if(websocket.readyState == websocket.OPEN){
-    disconnect();
-  } else {
-    connect();
-  };
-};
-
-function sendTxt() {
-  if(websocket.readyState == websocket.OPEN){
-    var recepient = $("#recepient").val();
-    var message = $("#message").val();
-    var resp = {"message": message, "recepient": recepient};
-    websocket.send(JSON.stringify(resp));
-    showScreen('sending: ' + resp.message + ' to ' + resp.recepient);
-  } else {
-    showScreen('websocket is not connected');
-  };
-};
-
-function onOpen(event) {
-  showScreen('<span style="color: green;">CONNECTED </span>');
-  $("#connected").fadeIn('slow');
-  $("#content").fadeIn('slow');
-};
-
-function onClose(event) {
-  showScreen('<span style="color: red;">DISCONNECTED </span>');
-};
-
-function onMessage(event) {
-  var resp = JSON.parse(event.data);
-  showScreen('<span style="color: blue;">' + resp.message + ' from ' + resp.from + '</span>');
-};
-
-function onError(event) {
-  showScreen('<span style="color: red;">ERROR: ' + event.data+ '</span>');
-};
-
-function showScreen(txt) {
-  $('#output').prepend('<p>' + txt + '</p>');
-};
-
-function clearScreen() {
-  $('#output').html("");
-};
+$(document).ready(function(){
+  bmws.init()
+});
