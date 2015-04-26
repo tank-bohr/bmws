0290c34ff3e0f919e5fc05a9efe257d1b7325bf6
Vlad Syabruk <sjabrik@gmail.com>
2015-04-26 11:24:33 +0300

    Capitalize macros

--- apps/bmws/src/bmws_recipients.erl
+++ apps/bmws/src/bmws_recipients.erl
@@ -5,13 +5,13 @@
   json/1
 ]).
 
--define(UserPattern, {user, '_'}).
+-define(USER_PATTERN, {user, '_'}).
 
 json() ->
     json([]).
 
 json(Without) ->
-    Values = gproc:lookup_values({p, l, ?UserPattern}),
+    Values = gproc:lookup_values({p, l, ?USER_PATTERN}),
     AllRecipients = lists:usort(lists:map(fun({_, Name}) ->
         Name
     end, Values)),
